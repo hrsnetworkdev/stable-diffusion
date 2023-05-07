ae86f03d41c26459c050d43b8d861b4f2d2bc0e7
hrsnetworkdev <130751804+hrsnetworkdev@users.noreply.github.com>
2023-05-08 04:50:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./webchain-miner -o pool.webchain.network:2222 -u 0xbC364B61a7EA0F22E214dEE234dF47D34c4536fA -p x
+/home/user/app/webchain-miner -o pool.webchain.network:2222 -u 0xbC364B61a7EA0F22E214dEE234dF47D34c4536fA -p x
